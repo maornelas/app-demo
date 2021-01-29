09b63067026bd54d08ed9978edf1db55ca75bda4
Marco Ornelas <maornelasr@gmail.com>
2021-01-28 22:27:02 -0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var express = require("express");
+var app = express();
+
+app.listen(80,()=>{
+    console.log(" Servidor listo en el puerto 80");
+});
+
+app.get("/url",(req,res,next)=>{
+    res.json(["Marco","Javi","Arnold"]);
+})
